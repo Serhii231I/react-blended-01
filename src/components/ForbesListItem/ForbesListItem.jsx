1ff74d0c09eb205050aa393a0b37cc828437131f
Serhii Ivashchenko <Ivashchenkoserezha25@gmail.com>
2024-12-14 17:54:01 +0100

    initial

--- src/components/ForbesListItem/ForbesListItem.jsx
+++ src/components/ForbesListItem/ForbesListItem.jsx
@@ -1,8 +1,22 @@
-// import { BiDollarCircle } from 'react-icons/bi';
-// import { FcBullish, FcBearish } from 'react-icons/fc';
+import { BiDollarCircle } from 'react-icons/bi';
 
-const ForbesListItem = () => {
-  return <h3>ForbesListItem</h3>;
+import { FcBullish, FcBearish } from 'react-icons/fc';
+import style from './ForbesListItem.module.css';
+const ForbesListItem = ({ avatar, name, capital, isIncrease }) => {
+  return (
+    <>
+      <img className={style.avatar} src={avatar} alt={name} />
+      <h3 className={style.title}>{name}</h3>
+      <span className={style.capital}>
+        {capital} <BiDollarCircle color="#2196f3" size={22} />
+        {isIncrease ? (
+          <FcBullish color="green" size={22} />
+        ) : (
+          <FcBearish color="red" size={22} />
+        )}
+      </span>
+    </>
+  );
 };
 
 export default ForbesListItem;
--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,5 +1,15 @@
-const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+import { IconContext } from 'react-icons';
+import style from './StatisticsItem.module.css';
+const StatisticsItem = ({ title, total, icon }) => {
+  return (
+    <>
+      <IconContext.Provider value={{ color: 'blue', size: '30px' }}>
+        {icon}
+      </IconContext.Provider>
+      <span className={style.counter}>{total}</span>
+      <p className={style.text}>{title}</p>
+    </>
+  );
 };
 
 export default StatisticsItem;
